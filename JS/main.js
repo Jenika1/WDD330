cd1d4c89f7f109ab10ef49817d15ed8affb96d90
JenikaH <59703597+JenikaH@users.noreply.github.com>
2020-09-28 12:52:52 -0600

    ch2

--- JS/main.js
+++ JS/main.js
@@ -5,6 +5,10 @@ const links = [{
     {
         label: "Week 2 notes",
         url: "week2/index.html"
+    },
+    {
+        label: "Week 3 notes",
+        url: "week3/index.html"
     }
 ]
 
---
+++ week3/js/objectmethod.js
@@ -0,0 +1,10 @@
+let user = {
+    name: "John",
+    age: 30
+  };
+  
+  user.sayHi = function() {
+    alert("Hello!");
+  };
+  
+  user.sayHi(); // Hello!
