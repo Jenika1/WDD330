6deeca4aef43c7b575fb638a1beaeaea43e89317
JenikaH <59703597+JenikaH@users.noreply.github.com>
2020-10-10 21:55:29 -0600

    Week 4

--- JS/main.js
+++ JS/main.js
@@ -9,6 +9,10 @@ const links = [{
     {
         label: "Week 3 notes",
         url: "week3/index.html"
+    },
+    {
+        label: "Week 4 notes",
+        url: "week4/index.html"
     }
 ]
 
--- week4/js/tictactoe.js
+++ week4/js/tictactoe.js
@@ -1,5 +1,10 @@
-//table loop three rows div
+//refrence the table div and declare a constant
+const table = document.querySelectorAll('.table');
+//creat a loop that adds 3 div with the class of row
 
 //rows loop three columns div
 
 //end at row 3 column 3
+const column = document.querySelectorAll('.col1', '.col2', '.col3');
+ document.addEventListener("click", doSomething);
+ 
