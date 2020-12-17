a7d7b8f5ae6cf4a600ac8d743d8d51096282f54d
JenikaH <59703597+JenikaH@users.noreply.github.com>
2020-12-16 19:00:47 -0700

    connect four

---
+++ week12/connect4/main.js
@@ -0,0 +1,55 @@
+// make this dynamic somehow...for (let i = 1; i<8; i++){}
+// I could not get this to work using getElementByClassName().
+let btn1 = document.getElementById("click1");
+let btn2 = document.getElementById("click2");
+let btn3 = document.getElementById("click3");
+let btn4 = document.getElementById("click4");
+let btn5 = document.getElementById("click5");
+let btn6 = document.getElementById("click6");
+let btn7 = document.getElementById("click7");
+
+btn1.addEventListener('click', function () {
+   //find the last box in first column that is not "taken" let box = "box";
+    let box = "box";
+    for (let i = 6; i > 0; i--) {
+        let v = box + i;
+        let div = document.getElementsByClassName(v);
+        if 
+    }
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+btn2.addEventListener('click', function () {
+    alert('You Clicked!');
+});
+btn3.addEventListener('click', function () {
+    alert('You Clicked!');
+});
+btn4.addEventListener('click', function () {
+    alert('You Clicked!');
+});
+btn5.addEventListener('click', function () {
+    alert('You Clicked!');
+});
+btn6.addEventListener('click', function () {
+    alert('You Clicked!');
+});
+btn7.addEventListener('click', function () {
+    alert('You Clicked!');
+});
